4687dfdea4b7c8df0e371d37320da85d7476f71e
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2021-08-17 21:00:51 -0400

    generate multiple cards by mapping over grid items + add footer

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,24 @@ import {
 } from "@material-ui/core";
 
 import { PhotoCamera } from "@material-ui/icons";
+import Link from "@material-ui/core/Link";
 
 import useStyles from "./styles";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const App = () => {
   const classes = useStyles();
@@ -67,37 +83,47 @@ const App = () => {
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
-            <Grid item>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://source.unsplash.com/random"
-                  title="image title"
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography variant="h5" gutterBottom>
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. Used to describe content
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    {" "}
-                    View
-                  </Button>
-                  <Button size="small" color="primary">
-                    {" "}
-                    Edit
-                  </Button>{" "}
-                  <Button> </Button>
-                </CardActions>
-              </Card>
-            </Grid>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. Used to describe content
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      {" "}
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      {" "}
+                      Edit
+                    </Button>{" "}
+                    <Button> </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Container>
       </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          <Copyright />
+        </Typography>
+      </footer>
     </>
   );
 };
